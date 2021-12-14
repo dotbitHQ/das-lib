bb1df68a7168d1b63e46ad194cdf4b83bacbb2c4
specerxi <xhxpecer@gmail.com>
2021-12-14 10:02:05 +0800

    fix: complex typed data sign (EIP-712)

--- sign/eth.go
+++ sign/eth.go
@@ -5,19 +5,27 @@ import (
 	"errors"
 	"fmt"
 	"github.com/DeAccountSystems/das-lib/common"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/signer/core"
-	"math/big"
+	"github.com/pranksteess/go-ethereum/crypto"
+	"github.com/pranksteess/go-ethereum/signer/core"
 )
+func EthSignature(data []byte, hexPrivateKey string) ([]byte, error) {
+	if len(data) == 0 {
+		return nil, errors.New("invalid raw data")
+	}
+	key, err := crypto.HexToECDSA(hexPrivateKey)
+	if err != nil {
+		return nil, err
+	}
 
-func EthSignature(signType bool, data string, hexPrivateKey string) ([]byte, error) {
+	return crypto.Sign(data, key)
+}
+
+func PersonalSignature(data string, hexPrivateKey string) ([]byte, error) {
 	if len(data) == 0 {
 		return nil, errors.New("invalid raw data")
 	}
 
-	if signType {
-		data = common.EthMessageHeader + data
-	}
+	data = common.EthMessageHeader + data
 
 	key, err := crypto.HexToECDSA(hexPrivateKey)
 	if err != nil {
@@ -27,8 +35,7 @@ func EthSignature(signType bool, data string, hexPrivateKey string) ([]byte, err
 
 	return crypto.Sign(tmpHash, key)
 }
-
-func EthVerifySignature(signType bool, sign []byte, rawData string, address string) bool {
+func VerifyPersonalSignature(sign []byte, rawData string, address string) bool {
 	if len(sign) != 65 { // sign check
 		return false
 	}
@@ -37,8 +44,24 @@ func EthVerifySignature(signType bool, sign []byte, rawData string, address stri
 		sign[64] = sign[64] - 27
 	}
 
-	if signType {
-		rawData = common.EthMessageHeader + rawData
+	rawData = common.EthMessageHeader + rawData
+
+	pubKey, err := GetSignedPubKey(rawData, sign)
+	if err != nil {
+		return false
+	}
+
+	recoveredAddr := crypto.PubkeyToAddress(*pubKey)
+
+	return recoveredAddr.Hex() == address
+}
+func VerifyEthSignature(sign []byte, rawData string, address string) bool {
+	if len(sign) != 65 { // sign check
+		return false
+	}
+
+	if sign[64] >= 27 {
+		sign[64] = sign[64] - 27
 	}
 
 	pubKey, err := GetSignedPubKey(rawData, sign)
@@ -51,40 +74,34 @@ func EthVerifySignature(signType bool, sign []byte, rawData string, address stri
 	return recoveredAddr.Hex() == address
 }
 
-func EthSignature712(str string, obj *common.MMJsonObj, digest, hexPrivateKey string) ([]byte, error) {
-	obj.Message.Digest = digest
-	objData, _ := json.Marshal(obj)
-	var typedData core.TypedData
-	_ = json.Unmarshal(objData, &typedData)
-	err := json.Unmarshal([]byte(str), &typedData)
+func EIP712Signature(typedData core.TypedData, hexPrivateKey string) ([]byte, []byte, error) {
+
+	domainSeparator, err := typedData.HashStruct("EIP712Domain", typedData.Domain.Map())
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-
-	domainMap := typedData.Domain.Map()
-	domainMap["chainId"] = big.NewInt(5)
-	log.Info("typedData.Message:", typedData.Message)
-	log.Info("typedData.Domain.Map():", domainMap)
-
-	typedDataHash, _ := typedData.HashStruct(typedData.PrimaryType, typedData.Message)
-	domainSeparator, _ := typedData.HashStruct("EIP712Domain", domainMap)
+	typedDataHash, err := typedData.HashStruct(typedData.PrimaryType, typedData.Message)
+	if err != nil {
+		return nil, nil, err
+	}
+	fmt.Println("domainSeparator: ", common.Bytes2Hex(domainSeparator), "typedDataHash: ", common.Bytes2Hex(typedDataHash))
 	rawData := []byte(fmt.Sprintf("\x19\x01%s%s", string(domainSeparator), string(typedDataHash)))
 	dataHash := crypto.Keccak256(rawData)
-
 	key, err := crypto.HexToECDSA(hexPrivateKey)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
+
 	signature, err := crypto.Sign(dataHash, key)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	if signature[64] < 27 {
 		signature[64] += 27
 	}
 
-	return signature, nil
+	return dataHash, signature, nil
 }
 
 func EthVerifySignature712(obj *common.MMJsonObj, sign []byte, digest, address string) bool {
