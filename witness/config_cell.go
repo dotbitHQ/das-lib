719e631188cac6d193e3420a50bb1e551a840bc6
scorpiotzh <835598264@qq.com>
2021-12-28 17:31:19 +0800

    feat: add emoji

--- witness/config_cell.go
+++ witness/config_cell.go
@@ -21,6 +21,7 @@ type ConfigCellDataBuilder struct {
 	ConfigCellApply             *molecule.ConfigCellApply
 	ConfigCellRelease           *molecule.ConfigCellRelease
 	ConfigCellRecordKeys        []string
+	ConfigCellEmojis            []string
 }
 
 func ConfigCellDataBuilderRefByTypeArgs(builder *ConfigCellDataBuilder, tx *types.Transaction, configCellTypeArgs common.ConfigCellTypeArgs) error {
@@ -116,6 +117,12 @@ func ConfigCellDataBuilderRefByTypeArgs(builder *ConfigCellDataBuilder, tx *type
 			return fmt.Errorf("key name space len err: %s", err.Error())
 		}
 		builder.ConfigCellRecordKeys = strings.Split(string(configCellDataBys[4:dataLength-4]), string([]byte{0x00}))
+	case common.ConfigCellTypeArgsCharSetEmoji:
+		dataLength, err := molecule.Bytes2GoU32(configCellDataBys[:4])
+		if err != nil {
+			return fmt.Errorf("key name space len err: %s", err.Error())
+		}
+		builder.ConfigCellEmojis = strings.Split(string(configCellDataBys[4:dataLength]), string([]byte{0x00}))
 	}
 	return nil
 }
