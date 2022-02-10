ab7b04bca153a0d1e9662b06197c801925ad3271
scorpiotzh <835598264@qq.com>
2022-02-10 20:10:14 +0800

    feat: add ed25519 sign

--- example/mixin_test.go
+++ example/mixin_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/DeAccountSystems/das-lib/common"
 	"github.com/DeAccountSystems/das-lib/core"
+	"github.com/DeAccountSystems/das-lib/sign"
 	"testing"
 )
 
@@ -17,11 +18,16 @@ func TestMixin(t *testing.T) {
 	}
 	fmt.Println(common.Bytes2Hex(privateKey))
 	fmt.Println(common.Bytes2Hex(publicKey), len(common.Bytes2Hex(publicKey)), len(publicKey))
+	msg := common.Hex2Bytes("0x6d31bda56835b9c2d4876a53d611dfb58238aacf26ca00d3c8f5a2165c3f70cf")
+	res := ed25519.Sign(privateKey, msg)
+	fmt.Println(common.Bytes2Hex(res))
+	fmt.Println(ed25519.Verify(publicKey, msg, res))
+
 	// 0x70c756ecfa897dc71cbfce48931fbb261f2e593fe234902a57f36aa9c27dec17 66 32
-	args1 := "0x0515a33588908cF8Edb27D1AbE3852Bf287Abd38910515a33588908cF8Edb27D1AbE3852Bf287Abd3891"
-	args2 := "0x0670c756ecfa897dc71cbfce48931fbb261f2e593fe234902a57f36aa9c27dec170670c756ecfa897dc71cbfce48931fbb261f2e593fe234902a57f36aa9c27dec17"
-	fmt.Println(len(args1), len(args2))
-	fmt.Println(len(common.Hex2Bytes(args1)), len(common.Hex2Bytes(args2)))
+	//args1 := "0x0515a33588908cF8Edb27D1AbE3852Bf287Abd38910515a33588908cF8Edb27D1AbE3852Bf287Abd3891"
+	//args2 := "0x0670c756ecfa897dc71cbfce48931fbb261f2e593fe234902a57f36aa9c27dec170670c756ecfa897dc71cbfce48931fbb261f2e593fe234902a57f36aa9c27dec17"
+	//fmt.Println(len(args1), len(args2))
+	//fmt.Println(len(common.Hex2Bytes(args1)), len(common.Hex2Bytes(args2)))
 }
 
 func TestFormatDasLockToOwnerAndManager(t *testing.T) {
@@ -54,4 +60,14 @@ func TestFormatDasLockToHexAddress(t *testing.T) {
 	fmt.Println(core.FormatDasLockToHexAddress(common.Hex2Bytes(args)))
 	args = "0x0670c756ecfa897dc71cbfce48931fbb261f2e593fe234902a57f36aa9c27dec170515a33588908cF8Edb27D1AbE3852Bf287Abd3891"
 	fmt.Println(core.FormatDasLockToHexAddress(common.Hex2Bytes(args)))
+}
+
+func TestEd25519Signature(t *testing.T) {
+	private := common.Hex2Bytes("0xf2011f49d9ad51fe64ce0f03afcff509e0324a046d8ef9b509805678fd2d9254e1090ce82474cbe0b196d1e62ec349ec05a61076c68d14129265370ca7e051c4")
+	msg := common.Hex2Bytes("0x01d4a002a560595c5e6f828c78eb7aafae23544f1c48263b76a0be3bfce219bd")
+	sin := sign.Ed25519Signature(private, msg)
+	fmt.Println(common.Bytes2Hex(sin))
+	//sin = common.Hex2Bytes("0x4d0fff8474b060546d7cd5310ba317412e100c53bad5a15665052e344b615f979bf951666276d8e1548d2a39b899f518d2d6718ad4bfe4ee2b6bc988b049bd0d")
+	pub := common.Hex2Bytes("0xe1090ce82474cbe0b196d1e62ec349ec05a61076c68d14129265370ca7e051c4")
+	fmt.Println(sign.VerifyEd25519Signature(pub, msg, sin))
 }
