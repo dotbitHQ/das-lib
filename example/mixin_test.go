e19108227c60e581fa1d8c62783eac4fc0b8efae
scorpiotzh <835598264@qq.com>
2022-02-14 09:56:31 +0800

    test: test mixin

--- example/mixin_test.go
+++ example/mixin_test.go
@@ -64,10 +64,14 @@ func TestFormatDasLockToHexAddress(t *testing.T) {
 
 func TestEd25519Signature(t *testing.T) {
 	private := common.Hex2Bytes("0xf2011f49d9ad51fe64ce0f03afcff509e0324a046d8ef9b509805678fd2d9254e1090ce82474cbe0b196d1e62ec349ec05a61076c68d14129265370ca7e051c4")
-	msg := common.Hex2Bytes("0x01d4a002a560595c5e6f828c78eb7aafae23544f1c48263b76a0be3bfce219bd")
+	msg := common.Hex2Bytes("0x6d31bda56835b9c2d4876a53d611dfb58238aacf26ca00d3c8f5a2165c3f70cf")
 	sin := sign.Ed25519Signature(private, msg)
 	fmt.Println(common.Bytes2Hex(sin))
 	//sin = common.Hex2Bytes("0x4d0fff8474b060546d7cd5310ba317412e100c53bad5a15665052e344b615f979bf951666276d8e1548d2a39b899f518d2d6718ad4bfe4ee2b6bc988b049bd0d")
 	pub := common.Hex2Bytes("0xe1090ce82474cbe0b196d1e62ec349ec05a61076c68d14129265370ca7e051c4")
 	fmt.Println(sign.VerifyEd25519Signature(pub, msg, sin))
+}
+
+func TestReverse(t *testing.T) {
+	fmt.Println(string(common.Hex2Bytes("0x313233343536373837322e626974")))
 }
