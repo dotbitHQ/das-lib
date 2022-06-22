c000adaf71e944d0003d97b49ef73a4ec48a7acb
scorpiotzh <835598264@qq.com>
2022-06-22 15:59:47 +0800

    feat: update key info

--- common/key_info.go
+++ common/key_info.go
@@ -1,10 +1,5 @@
 package common
 
-import (
-	"fmt"
-	"github.com/DeAccountSystems/das-lib/core"
-)
-
 type CoinType string // EIP-155
 
 const (
@@ -13,16 +8,6 @@ const (
 	CoinTypeBNB   CoinType = "714"
 	CoinTypeMatic CoinType = "966"
 )
-
-func FormatCoinTypeToDasChainType(coinType CoinType) ChainType {
-	switch coinType {
-	case CoinTypeEth, CoinTypeBNB, CoinTypeMatic:
-		return ChainTypeEth
-	case CoinTypeTrx:
-		return ChainTypeTron
-	}
-	return -1
-}
 
 type ChainId string //BIP-44
 
@@ -35,6 +20,16 @@ const (
 	ChainIdBscTestNet     ChainId = "97"
 	ChainIdPolygonTestNet ChainId = "80001"
 )
+
+func FormatCoinTypeToDasChainType(coinType CoinType) ChainType {
+	switch coinType {
+	case CoinTypeEth, CoinTypeBNB, CoinTypeMatic:
+		return ChainTypeEth
+	case CoinTypeTrx:
+		return ChainTypeTron
+	}
+	return -1
+}
 
 func FormatChainIdToDasChainType(netType DasNetType, chainId ChainId) ChainType {
 	if netType == DasNetTypeMainNet {
@@ -49,40 +44,4 @@ func FormatChainIdToDasChainType(netType DasNetType, chainId ChainId) ChainType 
 		}
 	}
 	return -1
-}
-
-type ChainTypeAddress struct {
-	Type    string  `json:"type"` // blockchain
-	KeyInfo KeyInfo `json:"key_info"`
-}
-
-type KeyInfo struct {
-	CoinType CoinType `json:"coin_type"`
-	ChainId  ChainId  `json:"chain_id"`
-	Key      string   `json:"key"`
-}
-
-func (c *ChainTypeAddress) FormatChainTypeAddress(net DasNetType, is712 bool) (*core.DasAddressHex, error) {
-	if c.Type != "blockchain" {
-		return nil, fmt.Errorf("not support type[%s]", c.Type)
-	}
-	dasChainType := FormatCoinTypeToDasChainType(c.KeyInfo.CoinType)
-	if dasChainType == -1 {
-		dasChainType = FormatChainIdToDasChainType(net, c.KeyInfo.ChainId)
-	}
-	if dasChainType == -1 {
-		return nil, fmt.Errorf("not support coin type[%s]-chain id[%s]", c.KeyInfo.CoinType, c.KeyInfo.ChainId)
-	}
-
-	daf := core.DasAddressFormat{DasNetType: net}
-	addrHex, err := daf.NormalToHex(core.DasAddressNormal{
-		ChainType:     dasChainType,
-		AddressNormal: c.KeyInfo.Key,
-		Is712:         is712,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("address NormalToHex err: %s", err.Error())
-	}
-
-	return &addrHex, nil
 }
