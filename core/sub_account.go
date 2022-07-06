ea5484f44e01fbef247667ad921fbd376f87852f
scorpiotzh <835598264@qq.com>
2022-07-06 13:05:29 +0800

    feat: add GetSubAccountCell

--- core/sub_account.go
+++ core/sub_account.go
@@ -8,6 +8,28 @@ import (
 	"github.com/nervosnetwork/ckb-sdk-go/types"
 )
 
+func (d *DasCore) GetSubAccountCell(parentAccountId string) (*indexer.LiveCell, error) {
+	contractSubAcc, err := GetDasContractInfo(common.DASContractNameSubAccountCellType)
+	if err != nil {
+		return nil, fmt.Errorf("GetDasContractInfo err: %s", err.Error())
+	}
+
+	searchKey := indexer.SearchKey{
+		Script:     contractSubAcc.ToScript(common.Hex2Bytes(parentAccountId)),
+		ScriptType: indexer.ScriptTypeType,
+		ArgsLen:    0,
+		Filter:     nil,
+	}
+	subAccLiveCells, err := d.client.GetCells(d.ctx, &searchKey, indexer.SearchOrderDesc, 1, "")
+	if err != nil {
+		return nil, fmt.Errorf("GetCells err: %s", err.Error())
+	}
+	if subLen := len(subAccLiveCells.Objects); subLen != 1 {
+		return nil, fmt.Errorf("sub account cell len: %d", subLen)
+	}
+	return subAccLiveCells.Objects[0], nil
+}
+
 func (d *DasCore) GetCustomScriptLiveCell(data []byte) (*indexer.LiveCell, error) {
 	subDataDetail := witness.ConvertSubAccountCellOutputData(data)
 	var customScript *types.Script
