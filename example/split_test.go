c30f547d266a9654c5b67c105bcf75664c90f65b
scorpiotzh <835598264@qq.com>
2022-10-31 15:09:16 +0800

    feat: add SplitOutputCell2 for split big cell

--- example/split_test.go
+++ example/split_test.go
@@ -4,15 +4,16 @@ import (
 	"fmt"
 	"github.com/dotbitHQ/das-lib/common"
 	"github.com/dotbitHQ/das-lib/core"
+	"github.com/nervosnetwork/ckb-sdk-go/indexer"
 	"testing"
 )
 
 func TestSplitOutputCell2(t *testing.T) {
-	total := 122 * common.OneCkb //10000000 * common.OneCkb
+	total := 10000 * common.OneCkb //10000000 * common.OneCkb
 	base := 61 * common.OneCkb
 
 	lockScript := common.GetNormalLockScript("0xc866479211cadf63ad115b9da50a6c16bd3d226d")
-	list, err := core.SplitOutputCell2(total, base, 1000, lockScript, nil)
+	list, err := core.SplitOutputCell2(total, base, 10, lockScript, nil, indexer.SearchOrderDesc)
 	if err != nil {
 		t.Fatal(err)
 	}
