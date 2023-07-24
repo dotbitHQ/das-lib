99c2a9630072c31d238dd691f6a485d9f38dfd1d
Jerry <i@sjis.me>
2023-07-24 10:30:26 +0800

    Revert "fix: AccountCell witness support create_approval action"
    This reverts commit 4e28b319d89006d8407f1da91341f4669fb37fcf.

--- witness/witness.go
+++ witness/witness.go
@@ -21,7 +21,6 @@ var (
 	DataEntityVersion1 = molecule.GoU32ToMoleculeU32(common.GoDataEntityVersion1)
 	DataEntityVersion2 = molecule.GoU32ToMoleculeU32(common.GoDataEntityVersion2)
 	DataEntityVersion3 = molecule.GoU32ToMoleculeU32(common.GoDataEntityVersion3)
-	DataEntityVersion4 = molecule.GoU32ToMoleculeU32(common.GoDataEntityVersion4)
 )
 
 const DasWitnessTagName = "witness"
