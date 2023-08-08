f83ef87af95d297f115f15f2dd31efe78edaea20
scorpiotzh <835598264@qq.com>
2023-08-08 17:15:23 +0800

    feat: Support remote sign

---
+++ remote_sign/remote_sign_v2.go
@@ -0,0 +1,138 @@
+package remote_sign
+
+import (
+	"bytes"
+	"encoding/hex"
+	"fmt"
+	"github.com/btcsuite/btcd/wire"
+	"github.com/dotbitHQ/das-lib/http_api"
+	"github.com/dotbitHQ/das-lib/sign"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/rlp"
+)
+
+type SignType int
+
+const (
+	SignTypeTx  SignType = 0
+	SignTypeMsg SignType = 1
+)
+
+type ReqRemoteSign struct {
+	SignType   SignType `json:"sign_type"`
+	Address    string   `json:"address"`
+	EvmChainID int64    `json:"evm_chain_id"`
+	Data       string   `json:"data"`
+}
+
+type RespRemoteSign struct {
+	Data string `json:"data"`
+}
+
+func RemoteSign(url string, req ReqRemoteSign) (*http_api.ApiResp, *RespRemoteSign, error) {
+	var data RespRemoteSign
+	resp, err := http_api.SendReqV2(url, &req, &data)
+	if err != nil {
+		return nil, nil, fmt.Errorf("http_api.SendReqV2 err: %s", err.Error())
+	}
+	return resp, &data, nil
+}
+
+func SignTxForCKB(url, addr string) sign.HandleSignCkbMessage {
+	return func(data string) ([]byte, error) {
+		resp, res, err := RemoteSign(url, ReqRemoteSign{
+			SignType:   SignTypeTx,
+			Address:    addr,
+			EvmChainID: 0,
+			Data:       data,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("RemoteSign err: %s", err.Error())
+		}
+		if resp.ErrNo != http_api.ApiCodeSuccess {
+			return nil, fmt.Errorf("RemoteSign fail code: %d, msg: %s", resp.ErrNo, resp.ErrMsg)
+		}
+		bys, err := hex.DecodeString(res.Data)
+		if err != nil {
+			return nil, fmt.Errorf("hex.DecodeString err: %s", err.Error())
+		}
+		return bys, nil
+	}
+}
+
+func SignTxForEVM(evmChainId int64, url, addr string, tx *types.Transaction) (*types.Transaction, error) {
+	dataBys, err := rlp.EncodeToBytes(tx)
+	if err != nil {
+		return nil, fmt.Errorf("rlp.EncodeToBytes err: %s", err.Error())
+	}
+
+	resp, res, err := RemoteSign(url, ReqRemoteSign{
+		SignType:   SignTypeTx,
+		Address:    addr,
+		EvmChainID: evmChainId,
+		Data:       hex.EncodeToString(dataBys),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("RemoteSign err: %s", err.Error())
+	}
+	if resp.ErrNo != http_api.ApiCodeSuccess {
+		return nil, fmt.Errorf("RemoteSign fail code: %d, msg: %s", resp.ErrNo, resp.ErrMsg)
+	}
+	bys, err := hex.DecodeString(res.Data)
+	if err != nil {
+		return nil, fmt.Errorf("hex.DecodeString err: %s", err.Error())
+	}
+	sigTx := &types.Transaction{}
+	if err = rlp.DecodeBytes(bys, sigTx); err != nil {
+		return nil, fmt.Errorf("rlp.DecodeBytes err: %s", err.Error())
+	}
+	return sigTx, nil
+}
+
+func SignTxForTRON(url, addr string, data []byte) ([]byte, error) {
+	resp, res, err := RemoteSign(url, ReqRemoteSign{
+		SignType:   SignTypeTx,
+		Address:    addr,
+		EvmChainID: 0,
+		Data:       hex.EncodeToString(data),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("RemoteSign err: %s", err.Error())
+	}
+	if resp.ErrNo != http_api.ApiCodeSuccess {
+		return nil, fmt.Errorf("RemoteSign fail code: %d, msg: %s", resp.ErrNo, resp.ErrMsg)
+	}
+	bys, err := hex.DecodeString(res.Data)
+	if err != nil {
+		return nil, fmt.Errorf("hex.DecodeString err: %s", err.Error())
+	}
+	return bys, nil
+}
+
+func SignTxForDOGE(url, addr string, tx *wire.MsgTx) (*wire.MsgTx, error) {
+	data := bytes.NewBuffer(make([]byte, 0, tx.SerializeSizeStripped()))
+	if err := tx.SerializeNoWitness(data); err != nil {
+		return nil, fmt.Errorf("SerializeNoWitness err: %s", err.Error())
+	}
+	resp, res, err := RemoteSign(url, ReqRemoteSign{
+		SignType:   SignTypeTx,
+		Address:    addr,
+		EvmChainID: 0,
+		Data:       hex.EncodeToString(data.Bytes()),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("RemoteSign err: %s", err.Error())
+	}
+	if resp.ErrNo != http_api.ApiCodeSuccess {
+		return nil, fmt.Errorf("RemoteSign fail code: %d, msg: %s", resp.ErrNo, resp.ErrMsg)
+	}
+	bys, err := hex.DecodeString(res.Data)
+	if err != nil {
+		return nil, fmt.Errorf("hex.DecodeString err: %s", err.Error())
+	}
+	var sigTx wire.MsgTx
+	if err = sigTx.DeserializeNoWitness(bytes.NewReader(bys)); err != nil {
+		return nil, fmt.Errorf("sigTx.DeserializeNoWitness err: %s", err.Error())
+	}
+	return &sigTx, nil
+}
