b722e63335fb48e824ae1ed97975e8ce0c9de2db
michael <457813723@qq.com>
2023-08-16 09:31:29 +0800

    update contract version and molecule lib

--- core/contract_status.go
+++ core/contract_status.go
@@ -12,15 +12,15 @@ var ContractStatusMapMainNet = map[common.DasContractName]common.ContractStatus{
 	common.DasContractNamePreAccountCellType:        {Version: "1.4.0"},
 	common.DasContractNameProposalCellType:          {Version: "1.3.0"},
 	common.DasContractNameConfigCellType:            {Version: "1.1.2"},
-	common.DasContractNameAccountCellType:           {Version: "1.7.1"},
+	common.DasContractNameAccountCellType:           {Version: "1.8.0"},
 	common.DasContractNameAccountSaleCellType:       {Version: "1.1.1"},
-	common.DASContractNameSubAccountCellType:        {Version: "1.3.1"},
+	common.DASContractNameSubAccountCellType:        {Version: "1.5.0"},
 	common.DASContractNameOfferCellType:             {Version: "1.0.1"},
 	common.DasContractNameBalanceCellType:           {Version: "1.3.0"},
 	common.DasContractNameIncomeCellType:            {Version: "1.2.1"},
 	common.DasContractNameReverseRecordCellType:     {Version: "1.1.0"},
 	common.DASContractNameEip712LibCellType:         {Version: "1.1.0"},
-	common.DasContractNameReverseRecordRootCellType: {Version: "1.0.0"},
+	common.DasContractNameReverseRecordRootCellType: {Version: "1.1.0"},
 }
 
 var ContractStatusMapTestNet = map[common.DasContractName]common.ContractStatus{
