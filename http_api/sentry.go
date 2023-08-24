b89e91ce3961a2b6727ff333db4bfa01f37072ab
michael <457813723@qq.com>
2023-08-24 22:10:30 +0800

    sentry

---
+++ http_api/sentry.go
@@ -0,0 +1,35 @@
+package http_api
+
+import (
+	"fmt"
+	"github.com/getsentry/sentry-go"
+	"time"
+)
+
+func SentryInit(dsn string) (err error) {
+	if dsn == "" {
+		return fmt.Errorf("sentry dsn is empty")
+	}
+	err = sentry.Init(sentry.ClientOptions{
+		Dsn:           dsn,
+		EnableTracing: true,
+		// Set TracesSampleRate to 1.0 to capture 100%
+		// of transactions for performance monitoring.
+		// We recommend adjusting this value in production,
+		TracesSampleRate: 1.0,
+	})
+	return
+
+}
+
+func RecoverPanic() {
+	if err := recover(); err != nil {
+		sentry.CurrentHub().Recover(err)
+		sentry.Flush(time.Second * 2)
+	}
+}
+
+func CaptureMessage(msg string) {
+	sentry.CurrentHub().CaptureMessage(msg)
+	sentry.Flush(time.Second * 2)
+}
