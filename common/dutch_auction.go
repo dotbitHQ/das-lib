9614160342090ac5cab5f0411614622ba2804433
michael <457813723@qq.com>
2023-11-20 11:53:43 +0800

    dutch auction

--- common/dutch_auction.go
+++ common/dutch_auction.go
@@ -2,7 +2,6 @@ package common
 
 import (
 	"fmt"
-	"github.com/dotbitHQ/das-lib/core"
 	"math"
 	"math/big"
 	"strconv"
@@ -10,18 +9,16 @@ import (
 
 const (
 	StartPremium int64 = 100000000
-	GRACE_PERIOD int64 = 90 * 24 * 3600
 )
 
-func Premium(expires, nowTime int64) float64 {
-	expires = expires + GRACE_PERIOD
-	if expires > nowTime {
+func Premium(startTime, nowTime int64) float64 {
+	if startTime > nowTime {
 		return float64(StartPremium)
 	}
 	numerator := new(big.Float).SetInt(new(big.Int).Exp(big.NewInt(10), big.NewInt(8), nil))
 	//2^(n/86400)
 	//n/86400
-	exponent := new(big.Float).Quo(big.NewFloat(float64(nowTime-expires)), big.NewFloat(86400))
+	exponent := new(big.Float).Quo(big.NewFloat(float64(nowTime-startTime)), big.NewFloat(86400))
 	exponentFloat, _ := exponent.Float64()
 	//2^(n/86400)
 	denominator := math.Pow(2, exponentFloat)
@@ -31,37 +28,4 @@ func Premium(expires, nowTime int64) float64 {
 	res, _ := result.Float64()
 	num, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", res), 64)
 	return num
-}
-
-type AuctionConfig struct {
-	GracePeriodTime, AuctionPeriodTime, DeliverPeriodTime uint32
-}
-
-func GetAuctionConfig(dasCore *core.DasCore) (res *AuctionConfig, err error) {
-	builderConfigCell, err := dasCore.ConfigCellDataBuilderByTypeArgs(ConfigCellTypeArgsAccount)
-	if err != nil {
-		err = fmt.Errorf("ConfigCellDataBuilderByTypeArgs err: %s", err.Error())
-		return
-	}
-	gracePeriodTime, err := builderConfigCell.ExpirationGracePeriod()
-	if err != nil {
-		err = fmt.Errorf("ExpirationGracePeriod err: %s", err.Error())
-		return
-	}
-	auctionPeriodTime, err := builderConfigCell.ExpirationAuctionPeriod()
-	if err != nil {
-		err = fmt.Errorf("ExpirationAuctionPeriod err: %s", err.Error())
-		return
-	}
-	deliverPeriodTime, err := builderConfigCell.ExpirationDeliverPeriod()
-	if err != nil {
-		err = fmt.Errorf("ExpirationDeliverPeriod err: %s", err.Error())
-		return
-	}
-	res = &AuctionConfig{
-		GracePeriodTime:   gracePeriodTime,
-		AuctionPeriodTime: auctionPeriodTime,
-		DeliverPeriodTime: deliverPeriodTime,
-	}
-	return
 }
