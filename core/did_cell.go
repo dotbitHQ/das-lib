0b4285b7fbcd3ff663b19ecc6bd1cc1da387b1af
scorpiotzh <835598264@qq.com>
2024-04-23 17:41:26 +0800

    feat: add func for build did cell tx

--- core/did_cell.go
+++ core/did_cell.go
@@ -105,7 +105,8 @@ type DidCellTxParams struct {
 	DidCellOutPoint     types.OutPoint
 	AccountCellOutPoint types.OutPoint
 
-	Records []witness.Record
+	EditRecords   []witness.Record
+	EditOwnerLock *types.Script
 }
 
 func (d *DasCore) BuildDidCellTx(p DidCellTxParams) (*txbuilder.BuildTransactionParams, error) {
@@ -118,9 +119,20 @@ func (d *DasCore) BuildDidCellTx(p DidCellTxParams) (*txbuilder.BuildTransaction
 		return d.BuildDidCellTxForEditRecords(p)
 	case common.DidCellActionEditOwner:
 		if p.DidCellOutPoint.TxHash.String() != "" {
-			// todo did cell -> did cell
+			// did cell -> did cell
+			return d.BuildDidCellTxForEditOwner(p)
 		} else if p.AccountCellOutPoint.TxHash.String() != "" {
-			// todo account cell -> did cell
+			// check EditOwnerLock is das lock
+			contractDispatch, err := GetDasContractInfo(common.DasContractNameDispatchCellType)
+			if err != nil {
+				return nil, fmt.Errorf("GetDasContractInfo err: %s", err.Error())
+			}
+			if contractDispatch.IsSameTypeId(p.EditOwnerLock.CodeHash) {
+				// account cell -> account cell
+				return d.BuildAccountCellTxForEditOwner(p)
+			}
+			// account cell -> did cell
+			return d.BuildDidCellTxForEditOwnerFromAccountCell(p)
 		} else {
 			return nil, fmt.Errorf("DidCellOutPoint and AccountCellOutPoint nil")
 		}
@@ -284,7 +296,7 @@ func (d *DasCore) BuildDidCellTxForEditRecords(p DidCellTxParams) (*txbuilder.Bu
 			Source: witness.SourceTypeOutputs,
 		},
 		ItemId:               didEntity.ItemId,
-		DidCellWitnessDataV0: &witness.DidCellWitnessDataV0{Records: p.Records},
+		DidCellWitnessDataV0: &witness.DidCellWitnessDataV0{Records: p.EditRecords},
 	}
 	outputsWitness, err := outputsDidEntity.ObjToBys()
 	if err != nil {
@@ -302,6 +314,116 @@ func (d *DasCore) BuildDidCellTxForEditRecords(p DidCellTxParams) (*txbuilder.Bu
 
 	// cell deps
 	txParams.CellDeps = append(txParams.CellDeps, timeCell.ToCellDep())
+
+	return &txParams, nil
+}
+
+func (d *DasCore) BuildDidCellTxForEditOwner(p DidCellTxParams) (*txbuilder.BuildTransactionParams, error) {
+	var txParams txbuilder.BuildTransactionParams
+
+	// check
+	if p.DidCellOutPoint.TxHash.String() == "" {
+		return nil, fmt.Errorf("DidCellOutPoint is nil")
+	}
+	didCellTx, err := d.client.GetTransaction(d.ctx, p.DidCellOutPoint.TxHash)
+	if err != nil {
+		return nil, fmt.Errorf("GetTransaction err: %s", err.Error())
+	}
+	didCellOutputs := didCellTx.Transaction.Outputs[p.DidCellOutPoint.Index]
+
+	// check did cell type
+	contractDidCell, err := GetDasContractInfo(common.DasContractNameDidCellType)
+	if err != nil {
+		return nil, fmt.Errorf("GetDasContractInfo err: %s", err.Error())
+	}
+	if !contractDidCell.IsSameTypeId(didCellOutputs.Type.CodeHash) {
+		return nil, fmt.Errorf("DidCellOutPoint is invalid: %s-%d", p.DidCellOutPoint.TxHash.String(), p.DidCellOutPoint.Index)
+	}
+
+	// check expire at
+	timeCell, err := d.GetTimeCell()
+	if err != nil {
+		return nil, fmt.Errorf("GetTimeCell err: %s", err.Error())
+	}
+
+	var didCellData witness.DidCellData
+	if err := didCellData.BysToObj(didCellTx.Transaction.OutputsData[p.DidCellOutPoint.Index]); err != nil {
+		return nil, fmt.Errorf("didCellData.BysToObj err: %s", err.Error())
+	}
+	if int64(didCellData.ExpireAt) < timeCell.Timestamp() {
+		return nil, fmt.Errorf("expired and unavailable")
+	}
+
+	// todo check owner lock
+
+	// inputs
+	txParams.Inputs = append(txParams.Inputs, &types.CellInput{
+		Since:          0,
+		PreviousOutput: &p.DidCellOutPoint,
+	})
+
+	// outputs
+	txParams.Outputs = append(txParams.Outputs, &types.CellOutput{
+		Capacity: didCellOutputs.Capacity,
+		Lock:     p.EditOwnerLock,
+		Type:     didCellOutputs.Type,
+	})
+
+	// witness
+	txDidEntity, err := witness.TxToDidEntity(didCellTx.Transaction)
+	if err != nil {
+		return nil, fmt.Errorf("witness.TxToDidEntity err: %s", err.Error())
+	}
+	didEntity, err := txDidEntity.GetDidEntity(witness.SourceTypeOutputs, uint64(p.DidCellOutPoint.Index))
+	if err != nil {
+		return nil, fmt.Errorf("txDidEntity.GetDidEntity err: %s", err.Error())
+	}
+
+	// inputs witness
+	inputsDidEntity := didEntity.ToInputsDidEntity(0)
+	inputsWitness, err := inputsDidEntity.ObjToBys()
+	if err != nil {
+		return nil, fmt.Errorf("inputsDidEntity.ObjToBys err: %s", err.Error())
+	}
+	txParams.Witnesses = append(txParams.Witnesses, inputsWitness)
+
+	// outputs witness
+	outputsDidEntity := witness.DidEntity{
+		Target: witness.CellMeta{
+			Index:  0,
+			Source: witness.SourceTypeOutputs,
+		},
+		ItemId:               didEntity.ItemId,
+		DidCellWitnessDataV0: &witness.DidCellWitnessDataV0{Records: nil},
+	}
+	outputsWitness, err := outputsDidEntity.ObjToBys()
+	if err != nil {
+		return nil, fmt.Errorf("outputsDidEntity.ObjToBys err: %s", err.Error())
+	}
+	txParams.Witnesses = append(txParams.Witnesses, outputsWitness)
+
+	// outputs data
+	didCellData.WitnessHash = outputsDidEntity.Hash()
+	outputsData, err := didCellData.ObjToBys()
+	if err != nil {
+		return nil, fmt.Errorf("didCellData.ObjToBys err: %s", err.Error())
+	}
+	txParams.OutputsData = append(txParams.OutputsData, outputsData)
+
+	// cell deps
+	txParams.CellDeps = append(txParams.CellDeps, timeCell.ToCellDep())
+
+	return &txParams, nil
+}
+
+func (d *DasCore) BuildDidCellTxForEditOwnerFromAccountCell(p DidCellTxParams) (*txbuilder.BuildTransactionParams, error) {
+	var txParams txbuilder.BuildTransactionParams
+
+	return &txParams, nil
+}
+
+func (d *DasCore) BuildAccountCellTxForEditOwner(p DidCellTxParams) (*txbuilder.BuildTransactionParams, error) {
+	var txParams txbuilder.BuildTransactionParams
 
 	return &txParams, nil
 }
