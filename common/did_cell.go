c4a2d622e40199d4d0e8cbabf6f2f2a668831f3d
scorpiotzh <835598264@qq.com>
2024-06-04 15:53:33 +0800

    feat: update for check address

--- common/did_cell.go
+++ common/did_cell.go
@@ -17,6 +17,8 @@ const (
 type AnyLockCodeHash = string
 
 const (
-	AnyLockCodeHashOfMainnetOmniLock AnyLockCodeHash = "0x9b819793a64463aed77c615d6cb226eea5487ccfc0783043a587254cda2b6f26"
-	AnyLockCodeHashOfTestnetOmniLock AnyLockCodeHash = "0xf329effd1c475a2978453c8600e1eaf0bc2087ee093c3ee64cc96ec6847752cb"
+	AnyLockCodeHashOfMainnetOmniLock  AnyLockCodeHash = "0x9b819793a64463aed77c615d6cb226eea5487ccfc0783043a587254cda2b6f26"
+	AnyLockCodeHashOfTestnetOmniLock  AnyLockCodeHash = "0xf329effd1c475a2978453c8600e1eaf0bc2087ee093c3ee64cc96ec6847752cb"
+	AnyLockCodeHashOfMainnetJoyIDLock AnyLockCodeHash = "0xd00c84f0ec8fd441c38bc3f87a371f547190f2fcff88e642bc5bf54b9e318323"
+	AnyLockCodeHashOfTestnetJoyIDLock AnyLockCodeHash = "0xd23761b364210735c19c60561d213fb3beae2fd6172743719eff6920e020baac"
 )
